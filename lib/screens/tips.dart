1c242cc758906d1dc52cb936ed654fc78cc5b9ec
Francesco Izzi <francesco.izzi@studenti.unipd.it>
2023-07-06 17:41:49 +0200

    usa questo per calendario

--- lib/screens/tips.dart
+++ lib/screens/tips.dart
@@ -5,17 +5,18 @@ import 'package:hypnos/suggestions/exercise/exercise_sets.dart';
 import 'package:hypnos/suggestions/meditation/meditation.dart';
 
 class TipsPage extends StatefulWidget {
-  const TipsPage({super.key});
+  const TipsPage({Key? key}) : super(key: key);
 
   static const routeDisplayname = 'TipsPage';
 
   @override
-  State<TipsPage> createState() => _TipsPage();
+  State<TipsPage> createState() => _TipsPageState();
 }
 
-class _TipsPage extends State<TipsPage> {
+class _TipsPageState extends State<TipsPage> {
  
   List<String> labels = ['Maditation','Exercises'];
+  List<String> texts = ['Catching your thoughts :','Exercise of the day :'];
   int _tabTextIndexSelected = 0;
 
   void _onTips(int index) {
@@ -23,7 +24,6 @@ class _TipsPage extends State<TipsPage> {
       _tabTextIndexSelected = index;
     });
   }
-
 
   Widget _selectLabel({
     required int index,
@@ -34,7 +34,7 @@ class _TipsPage extends State<TipsPage> {
       case 1:
         return buildWorkouts();
       default:
-        return const TipsPage();
+        return buildWorkouts();
     }
   }
 
@@ -43,56 +43,57 @@ class _TipsPage extends State<TipsPage> {
     return Scaffold(
       backgroundColor: const Color(0xFFE4DFD4),
       body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child:  Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Padding(padding: EdgeInsets.only(top: 40)),
-              FlutterToggleTab(
-                width: 90,
-                borderRadius: 30,
-                height: 50,
-                selectedIndex: _tabTextIndexSelected,
-                selectedBackgroundColors: const [Colors.blue, Colors.blueAccent],
-                selectedTextStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                ),
-                unSelectedTextStyle: const TextStyle(
-                  color: Colors.black87,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500
-                ),
-                labels: labels,
-                selectedLabelIndex: _onTips,
-                isScroll: false,
+        child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(padding: EdgeInsets.only(top: 50)),
+                  FlutterToggleTab(
+                    width: 90,
+                    borderRadius: 30,
+                    height: 50,
+                    selectedIndex: _tabTextIndexSelected,
+                    selectedBackgroundColors: const [Colors.blue, Colors.blueAccent],
+                    selectedTextStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    unSelectedTextStyle: const TextStyle(
+                      color: Colors.black87,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500
+                    ),
+                    labels: labels,
+                    selectedLabelIndex: _onTips,
+                    isScroll: false,
+                  ),
+                  const SizedBox(height: 45),
+                  Text(
+                  texts[_tabTextIndexSelected],
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  const SizedBox(height: 8),
+                  _selectLabel(index: _tabTextIndexSelected),
+                ],
               ),
-              const SizedBox(height: 45),
-              const Text(
-                'Exercises of the day:',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              const SizedBox(height: 8),
-              _selectLabel(index: _tabTextIndexSelected),
-            ],
-          ),
-        ),
+      ),
     ); 
   }
 
-    Widget buildWorkouts() => GestureDetector(
-        child: Column(
-          children:
-          exerciseSets
-              .map(
-                (exerciseSet) => Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10),
-                    child: ExerciseSetWidget(exerciseSet: exerciseSet)),
-              )
-              .toList(),
-        ),
-      );
+  Widget buildWorkouts() {
+    return Column(
+      children: exerciseSets.map((exerciseSet) {
+        return Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          child: ExerciseSetWidget(exerciseSet: exerciseSet),
+        );
+      }).toList(),
+    );
+  }
 
-      Widget buildMeditation() => const Meditation();
+  Widget buildMeditation() {
+    return const Meditation();
+  }
+
 }
